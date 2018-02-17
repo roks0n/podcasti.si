86b4ef779708dd607db801935c64db8418aada22
Jure Stolnik <jure.stolnik@gmail.com>
2018-02-17 21:37:54 +0000

    fixed getting parser class

--- src/podcasts/modules/sync.py
+++ src/podcasts/modules/sync.py
@@ -2,11 +2,19 @@ from podcasts.models import Episode
 from podcasts.modules.parsers.base import BasePodcastParser, DefaultPodcastParser, SoundcloudParser
 
 
-def get_parser(cls, class_name):
-    for subclass in cls.__subclasses__():
-        get_parser(subclass, class_name)
-        if subclass.__name__.lower() == class_name.lower():
-            return subclass
+def get_parser(base_class, class_name):
+
+    def all_parser_classes(cls):
+        for subclass in cls.__subclasses__():
+            yield from all_parser_classes(subclass)
+            yield subclass
+
+    return next(
+        (
+            cls for cls in all_parser_classes(base_class)
+            if cls.__name__.lower() == class_name.lower()
+        ), None
+    )
 
 
 def sync_podcast(podcast):
