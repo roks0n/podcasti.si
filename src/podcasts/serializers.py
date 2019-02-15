5ef5c7631c21ec45990fb0016a0fea08eb645ad3
ambrozic <ambrozic@gmail.com>
2019-02-15 20:42:06 +0000

    serializers

---
+++ src/podcasts/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from podcasts import models
+
+
+class EpisodeSerializer(serializers.ModelSerializer):
+    _url = serializers.HyperlinkedIdentityField(view_name='episode-detail', lookup_field='slug', read_only=True)
+
+    class Meta:
+        model = models.Episode
+        fields = '__all__'
+        exclude = ()
+
+
+class PodcastSerializer(serializers.ModelSerializer):
+    _url = serializers.HyperlinkedIdentityField(view_name='podcast-detail', lookup_field='slug', read_only=True)
+
+    class Meta:
+        model = models.Podcast
+        fields = '__all__'
+        exclude = ()
+
+
+class FeedSerializer(serializers.ModelSerializer):
+    _url = serializers.HyperlinkedIdentityField(view_name='episode-detail', lookup_field='slug', read_only=True)
+    podcast = PodcastSerializer()
+
+    class Meta:
+        model = models.Episode
+        fields = '__all__'
+        exclude = ()
