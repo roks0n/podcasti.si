d8b8806c2b0ea847ee9aa71d6f44e086a2a3f1c8
roks0n <haluzan.rok@gmail.com>
2019-04-06 20:21:02 +0200

    set PYTHONPATH in config.yml

--- tests/podcasts/test_views.py
+++ tests/podcasts/test_views.py
@@ -3,4 +3,4 @@ from django.urls import reverse
 
 def test_health_200(client):
     response = client.get(reverse("health"))
-    assert response.status_code == 123
+    assert response.status_code == 200
