20d89fb45ca3e1d070ecfd514ec9ff9a40675b98
roks0n <haluzan.rok@gmail.com>
2019-04-06 20:04:11 +0200

    failed tests check

--- tests/podcasts/test_views.py
+++ tests/podcasts/test_views.py
@@ -3,4 +3,4 @@ from django.urls import reverse
 
 def test_health_200(client):
     response = client.get(reverse("health"))
-    assert response.status_code == 200
+    assert response.status_code == 123
