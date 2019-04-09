3650865eee4224f078d5b61cb1aaeaa25a7c4f8d
roks0n <haluzan.rok@gmail.com>
2019-04-09 23:00:24 +0200

    fix "time" language

--- src/podcasts/utils/time.py
+++ src/podcasts/utils/time.py
@@ -19,21 +19,26 @@ def pretty_date(datetime):
         if second_diff < 10:
             output = "ravnokar"
         elif second_diff < 60:
-            output = "{} sekund nazaj".format(second_diff)
+            output = "{} sekundami".format(second_diff)
         elif second_diff < 120:
             return "pred minuto"
         elif second_diff < 3600:
-            output = "{} minut nazaj".format(round(second_diff / 60))
+            output = "pred manj kot uro"
         elif second_diff < 7200:
-            output = "pred eno uro"
+            output = "pred 1 uro"
+        elif second_diff < 10800:
+            output = "pred 2 urama"
         elif second_diff < 86400:
-            output = "{} ur nazaj".format(round(second_diff / 3600))
+            output = "pred {} urami".format(round(second_diff / 3600))
 
     elif day_diff == 1:
-        output = "Včeraj"
+        output = "včeraj"
+
+    elif day_diff < 3:
+        output = "pred 2 dnevoma"
 
     elif day_diff < 30:
-        output = "{} dni nazaj".format(day_diff)
+        output = "pred {} dnevi".format(day_diff)
 
     if not output:
         output = datetime.strftime("%d.%m.%Y")
