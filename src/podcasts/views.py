18b3bb048bbfae50fcb0831a4d08c689e1d580ef
roks0n <haluzan.rok@gmail.com>
2020-03-30 23:45:07 +0200

    derp take 3 💤

--- src/podcasts/views.py
+++ src/podcasts/views.py
@@ -127,6 +127,7 @@ class EpisodeView(TemplateView):
 
     def get_context_data(self, podcast_slug, episode_slug, **kwargs):
         context = super().get_context_data(**kwargs)
+        
         episode_qs = Episode.objects.filter(slug=episode_slug, podcast__slug=podcast_slug)
         if not episode_qs.exists():
             raise Http404
@@ -138,7 +139,7 @@ class EpisodeView(TemplateView):
             episode = episode_qs.get()
 
         track_episode(episode)
-        track_podcast(podcast)
+        track_podcast(episode.podcast)
 
         context.update(
             {
